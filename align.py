09753836942f3151c67fdadb935d69669585488f
Fischer Jemison <jemisonf@oregonstate.edu>
2019-02-20 22:14:13 -0800

    adds gitignore and WIP align class

--- align.py
+++ align.py
@@ -1,42 +1,57 @@
+from utils import AlignmentUtils
+
+# test function please ignore -- will delete in final
 def print_matrix(matrix):
     for row in matrix:
         for index in row:
             print (f"{index} ", end="")
         print("\n", end="")
 
-def diff(char_i, char_j):
-    return 10
+class Aligner:
+    def __init__(self, cost_filename='imp2cost.txt'):
+        self.utils = AlignmentUtils(cost_filename)
+        self.D = []
 
-def align(seq_1, seq_2):
-    D = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
-    ptr = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
-    # 0 = LEFT
-    # 1 = DOWN
-    # 2 = DIAG
-    print(D)
-    for i in range(len(seq_1) + 1):
-         D[i][0] = i
-    for j in range(len(seq_2) + 1):
-         D[0][j] = j
+    def diff(self, char_i, char_j):
+        return self.utils.get_cost(char_i, char_j)
 
-    for i in range(1, len(seq_1) + 1):
-        for j in range(1, len(seq_2) + 1):
-            D[i][j] = min(
-                            D[i - 1][j] + 1,
-                            D[i][j - 1] + 1,
-                            D[i - 1][j - 1] + diff(seq_1[i - 1], seq_2[j - 1])
-            )
-            if (D[i - 1][j] + 1 < D[i][j - 1] + 1 and 
-                D[i - 1][j] + 1 < D[i - 1][j - 1] + diff(seq_1[i - 1], seq_2[j - 1])):
-                ptr[i][j] = 0
-            if (D[i][j - 1] + 1 < D[i - 1][j] + 1 and 
-                D[i][j - 1] + 1 < D[i - 1][j - 1] + diff(seq_1[i - 1], seq_2[j - 1])):
-                ptr[i][j] = 1
-            else:
-                ptr[i][j] = 2
-    return D[len(seq_1)][len(seq_2)], ptr
+    def deletion(self, i, j):
+        return self.D[i - 1][j] + 1
+
+    def insertion(self, i, j):
+        return self.D[i][j - 1] + 1
+
+    def alignment(self, i, j, seq_1, seq_2):
+        return self.D[i - 1][j - 1] + self.diff(seq_1[i - 1], seq_2[j - 1])
+
+    def align(self, seq_1, seq_2):
+        self.D = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
+        ptr = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
+        # 0 = LEFT
+        # 1 = DOWN
+        # 2 = DIAG
+        for i in range(len(seq_1) + 1):
+            self.D[i][0] = i
+        for j in range(len(seq_2) + 1):
+            self.D[0][j] = j
+
+        for i in range(1, len(seq_1) + 1):
+            for j in range(1, len(seq_2) + 1):
+                self.D[i][j] = min(
+                                self.deletion(i, j),
+                                self.insertion(i, j),
+                                self.alignment(i, j, seq_1, seq_2)
+                )
+                if (self.deletion(i, j) < self.insertion(i, j) and self.deletion(i, j) < self.alignment(i, j, seq_1, seq_2)):
+                    ptr[i][j] = 0
+                if (self.insertion(i, j) < self.deletion(i, j) and self.insertion(i, j) < self.alignment(i, j, seq_1, seq_2)):
+                    ptr[i][j] = 1
+                else:
+                    ptr[i][j] = 2
+        return self.D[len(seq_1)][len(seq_2)], ptr
 
 
-n, D = align("ata", "atga")
-print(n)
-print_matrix(D)
+# aligner = Aligner()
+# n, D = aligner.align("ATCC", "TCAC")
+# print(n)
+# print_matrix(D)

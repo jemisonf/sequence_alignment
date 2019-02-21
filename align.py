bfcf916a48faa4e9c28bdfb6f8938f98087f77d2
Fischer Jemison <jemisonf@oregonstate.edu>
2019-02-20 21:30:00 -0800

    creates rough align code
    adds gitignore and WIP align class
    
    fixes ptr notation
    
    updates ptr numbers
    
    creates test
    
    fixes test
    
    fixes to align
    
    updates test, aling
    
    working align algo

---
+++ align.py
@@ -0,0 +1,55 @@
+from utils import AlignmentUtils
+
+# test function please ignore -- will delete in final
+def print_matrix(matrix):
+    for row in matrix:
+        for index in row:
+            print (f"{index}\t", end="")
+        print("\n", end="")
+
+class Aligner:
+    def __init__(self, cost_filename='imp2cost.txt'):
+        self.utils = AlignmentUtils(cost_filename)
+        self.D = []
+
+    def diff(self, char_i, char_j):
+        return self.utils.get_cost(char_i, char_j)
+
+    def deletion(self, i, j, seq_1, seq_2):
+        return self.D[i - 1][j] + self.diff(seq_1[i - 1], '-')
+
+    def insertion(self, i, j, seq_1, seq_2):
+        return self.D[i][j - 1] + self.diff(seq_2[j - 1], '-')
+
+    def alignment(self, i, j, seq_1, seq_2):
+        return self.D[i - 1][j - 1] + self.diff(seq_1[i - 1], seq_2[j - 1])
+
+    def align(self, seq_1, seq_2):
+        self.D = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
+        ptr = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
+        # 0 = DELETION
+        # 1 = INSERTION
+        # 2 = ALIGN
+        self.D[0][0] = 0
+        for i in range(1, len(seq_1) + 1):
+            self.D[i][0] = self.D[i - 1][0] + self.diff(seq_1[i - 1], '-')
+        for j in range(1, len(seq_2) + 1):
+            self.D[0][j] = self.D[0][j - 1] + self.diff(seq_2[j - 1], '-')
+
+        print_matrix(self.D)
+
+        for i in range(1, len(seq_1) + 1):
+            for j in range(1, len(seq_2) + 1):
+                self.D[i][j] = min(
+                                self.deletion(i, j, seq_1, seq_2),
+                                self.insertion(i, j, seq_1, seq_2),
+                                self.alignment(i, j, seq_1, seq_2)
+                )
+                if (self.D[i][j] == self.deletion(i, j, seq_1, seq_2)):
+                    ptr[i][j] = 0
+                elif (self.D[i][j] == self.insertion(i, j, seq_1, seq_2)):
+                    ptr[i][j] = 1
+                else:
+                    ptr[i][j] = 2
+        print_matrix(self.D)
+        return self.D[len(seq_1)][len(seq_2)], ptr

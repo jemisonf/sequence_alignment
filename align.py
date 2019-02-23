58df5bb3861aa2d2f3f60dde55b2b9c63d508171
Fischer Jemison <jemisonf@oregonstate.edu>
2019-02-22 18:33:32 -0800

    cleans up align code

--- align.py
+++ align.py
@@ -1,12 +1,5 @@
 from utils import AlignmentUtils
 
-# test function please ignore -- will delete in final
-def print_matrix(matrix):
-    for row in matrix:
-        for index in row:
-            print (f"{index}\t", end="")
-        print("\n", end="")
-
 class Aligner:
     def __init__(self, cost_filename='imp2cost.txt'):
         self.utils = AlignmentUtils(cost_filename)
@@ -24,6 +17,11 @@ class Aligner:
     def alignment(self, i, j, seq_1, seq_2):
         return self.D[i - 1][j - 1] + self.diff(seq_1[i - 1], seq_2[j - 1])
 
+    """Calculates the edit distance and backtrace matrix for two strings
+    
+    Returns:
+        int, [list[list]] -- edit distance, 2D array of values used for backtrace
+    """
     def align(self, seq_1, seq_2):
         self.D = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
         ptr = [[ 0 for j in range(len(seq_2) + 1)] for i in range(len(seq_1) + 1)]
@@ -35,8 +33,6 @@ class Aligner:
             self.D[i][0] = self.D[i - 1][0] + self.diff(seq_1[i - 1], '-')
         for j in range(1, len(seq_2) + 1):
             self.D[0][j] = self.D[0][j - 1] + self.diff(seq_2[j - 1], '-')
-
-        print_matrix(self.D)
 
         for i in range(1, len(seq_1) + 1):
             for j in range(1, len(seq_2) + 1):
@@ -51,5 +47,4 @@ class Aligner:
                     ptr[i][j] = 1
                 else:
                     ptr[i][j] = 2
-        print_matrix(self.D)
         return self.D[len(seq_1)][len(seq_2)], ptr
